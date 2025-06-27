3091f70eb9c064593554eacb41f788c723fe853d
elefsaria <mitraponsel5828@gmail.com>
2025-06-28 04:23:01 +0700

    Update FruitTile.jsx

--- src/components/FruitTile.jsx
+++ src/components/FruitTile.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default function FruitTile({ fruit, isBlasting }) {
+export default function FruitTile({ fruit, isBlasting, isSelected, onClick }) {
   return (
-    <div className={`fruit-tile ${isBlasting ? "blast" : ""}`}>
+    <div
+      className={`fruit-tile ${isBlasting ? "blast" : ""} ${
+        isSelected ? "selected" : ""
+      }`}
+      onClick={onClick}
+    >
       <img src={`/images/avatars/${fruit}.png`} alt={fruit} />
     </div>
   );
