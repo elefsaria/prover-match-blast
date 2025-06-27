7f824f2998a19c0c4ae4ad3e0ae0470338442abb
elefsaria <mitraponsel5828@gmail.com>
2025-06-28 03:42:59 +0700

    Update FruitTile.jsx

--- src/components/FruitTile.jsx
+++ src/components/FruitTile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FruitTile({ fruit, onClick }) {
   return (
     <div className="fruit-tile" onClick={onClick}>
-      <img src={`/images/fruits/${fruit}.png`} alt={fruit} />
+      <img src={`/images/avatars/${fruit}.png`} alt={fruit} />
     </div>
   );
 }
